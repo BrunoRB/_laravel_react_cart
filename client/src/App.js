3f14b39945794fc109450b89aeb620abd776408d
BrunoRB <brunorb8@hotmail.com>
2021-04-19 13:25:33 -0300

    simple aligment

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,49 @@
-import React from 'react'
-import { Switch, Route, BrowserRouter, Link } from 'react-router-dom'
-import { ChakraProvider } from "@chakra-ui/react"
+import React from "react";
+import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
-import ProductList from './pages/ProductList'
-import Cart from 'pages/Cart'
+import ProductList from "./pages/ProductList";
+import Cart from "pages/Cart";
 
-import { Divider } from "@chakra-ui/react"
-import { Button } from "@chakra-ui/react"
-import { IconButton } from "@chakra-ui/react"
-
+import { Divider } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { Flex, Spacer, Box } from "@chakra-ui/react";
 
 const AppRoutes = () => (
-    <BrowserRouter>
+  <BrowserRouter>
+    <Flex>
+      <Box p="4">
+        <Button colorScheme="green" size="lg">
+          <Link to="/">Home</Link>
+        </Button>
+      </Box>
+      <Spacer />
+      <Box p="4">
+        <Button colorScheme="blue" size="lg">
+          <Link to="/cart">Cart</Link>
+        </Button>
+      </Box>
+    </Flex>
 
-        <Button colorScheme="green"><Link to="/">Home</Link></Button>
-        <Button colorScheme="blue"><Link to="/cart">Cart</Link></Button>
+    <Divider orientation="horizontal" />
 
-        <Divider orientation="horizontal" />
-
-        <Switch>
-            <Route exact path="/">
-                <ProductList />
-            </Route>
-            <Route exact path="/cart">
-                <Cart />
-            </Route>
-        </Switch>
-    </BrowserRouter>
-)
+    <Switch>
+      <Route exact path="/">
+        <ProductList />
+      </Route>
+      <Route exact path="/cart">
+        <Cart />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 const App = () => {
-    return (
-      <ChakraProvider>
-        <AppRoutes />
-      </ChakraProvider>
-    )
-}
+  return (
+    <ChakraProvider>
+      <AppRoutes />
+    </ChakraProvider>
+  );
+};
 
 export default App;
