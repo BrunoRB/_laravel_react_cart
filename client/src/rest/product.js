458e6b68fa71d9c1ef8155eab803fbe0a7526d1a
BrunoRB <brunorb8@hotmail.com>
2021-04-19 13:19:33 -0300

    product data fetch

---
+++ client/src/rest/product.js
@@ -0,0 +1,24 @@
+
+
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://607c843867e653001757420d.mockapi.io/api'
+});
+
+const listProducts = (page, limit) => {
+    const url = `/products/products?p=${page}&l=${limit}`;
+
+    // TODO this is for testing only (bad connection and a slow API)
+    let data = sessionStorage.getItem(url);
+    if (!data) {
+        return instance.get(url).then(data => {
+            sessionStorage.setItem(url, JSON.stringify(data));
+            return data;
+        });
+    } else {
+        return Promise.resolve(JSON.parse(data));
+    }
+};
+
+export {listProducts};
