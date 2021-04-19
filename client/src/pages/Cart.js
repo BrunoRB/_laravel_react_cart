0d562ee1312f634ebaeadc992a0c84a13c59ba09
BrunoRB <brunorb8@hotmail.com>
2021-04-19 13:16:58 -0300

    missing imports

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import {
+    Alert,
+    AlertIcon,
+    AlertDescription,
+    Spinner
+  } from "@chakra-ui/react"
 
 
 const Cart = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-
+    const [cartList, setList] = useState([]);
 
     if (error) {
         return (
