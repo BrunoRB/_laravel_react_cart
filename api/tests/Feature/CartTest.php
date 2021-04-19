6b8a8273ac4ab49054a741596a1dd6be43915604
BrunoRB <brunorb8@hotmail.com>
2021-04-19 00:36:32 -0300

    full carttest draft

---
+++ api/tests/Feature/CartTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class CartTest extends TestCase
+{
+    use WithFaker;
+
+    public function testBasicAdd()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testAddMultipleTimes()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testListEmpty()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testList()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function setChangeAmount()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testChangeAmountInexistent()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testChangeToInvalidAmount()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testCheckoutEmptyCart()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testCheckout()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testDelete()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testInexistent()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+}
