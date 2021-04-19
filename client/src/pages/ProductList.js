16e14bf7e8392f7033fea5e31b68875c3cc0dcc9
BrunoRB <brunorb8@hotmail.com>
2021-04-19 13:27:49 -0300

    simple product listing (no layout)

--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -1,39 +1,71 @@
-import React, { useState } from 'react';
-import {
-    Alert,
-    AlertIcon,
-    AlertDescription,
-  } from "@chakra-ui/react"
+import React, { useEffect, useState } from "react";
+import { Alert, AlertIcon, AlertDescription } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 
+import { listProducts } from "rest/product";
 
 const ProductList = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [productList, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    if (error) {
-        return (
-            <>
-                <Alert status="error">
-                    <AlertIcon />
-                    <AlertDescription>{error}</AlertDescription>
-                </Alert>
-            </>
-        )
-    } else if (!productList.length && !isLoading) {
-        return (
-            <>
-                <Alert status="info">
-                    <AlertIcon />
-                    <AlertDescription>Nothing to show</AlertDescription>
-                </Alert>
-            </>
-        )
-    } else {
-        return (
-            <>
-            </>
-        )
-    }
-}
+  // pretend this is coming together with the "loadNext" data
+  const [page, setPage] = useState(1);
 
-export default ProductList
+  const loadNext = () => {
+    const limit = 5;
+    setIsLoading(true);
+    listProducts(page, limit)
+      .then((res) => {
+        setList(productList.concat(res.data));
+
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError("Something went wrong");
+        console.error(err);
+        setIsLoading(false);
+      });
+
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    loadNext();
+    return () => true;
+  }, []);
+
+  if (error) {
+    return (
+      <>
+        <Alert status="error">
+          <AlertIcon />
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      </>
+    );
+  } else if (!productList.length && !isLoading) {
+    return (
+      <>
+        <Alert status="info">
+          <AlertIcon />
+          <AlertDescription>Nothing to show</AlertDescription>
+        </Alert>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <SimpleGrid columns={5} spacing={10}>
+          {productList.map((product) => {
+            return (
+              <>{JSON.stringify(product)}</>
+            );
+          })}
+        </SimpleGrid>
+      </>
+    );
+  }
+};
+
+export default ProductList;
