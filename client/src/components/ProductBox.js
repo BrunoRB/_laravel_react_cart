3d966942ea98633249190af0c3a63b328d0c65d4
BrunoRB <brunorb8@hotmail.com>
2021-04-19 13:51:06 -0300

    full UI product listing

---
+++ client/src/components/ProductBox.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { addToCart } from "rest/cart";
+import { useToast } from "@chakra-ui/react";
+
+const ProductBox = (props) => {
+  const toast = useToast();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const handleAddToCart = () => {
+    setIsLoading(true);
+
+    addToCart(props.product)
+      .then((xs) => {
+        setIsLoading(false);
+        toast({
+          title: "Product added to cart",
+          description: "",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+
+        return xs;
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        toast({
+          title: "Something went wrong",
+          description: "",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      });
+  };
+
+  return (
+    <>
+      <Box maxW="sm" overflow="hidden">
+        {/* <Image src={props.product.imageUrl} alt={props.product.product} /> */}
+
+        <Box p="6">
+          <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
+            {props.product.productName}
+          </Box>
+
+          {props.displayAddToCart ? (
+            <Box>
+              {props.product.product}
+              <br />
+              ${props.product.price}
+              <div>
+              <Button
+                isLoading={isLoading}
+                loadingText="Adding"
+                colorScheme="teal"
+                variant="outline"
+                onClick={handleAddToCart}
+              >
+                Add to cart
+              </Button>
+              </div>
+            </Box>
+          ) : (
+            ""
+          )}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export { ProductBox };
--- client/src/rest/product.js
+++ client/src/rest/product.js
@@ -1,24 +1,22 @@
-
-
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://607c843867e653001757420d.mockapi.io/api'
+  baseURL: "https://607c843867e653001757420d.mockapi.io/api",
 });
 
 const listProducts = (page, limit) => {
-    const url = `/products/products?p=${page}&l=${limit}`;
+  const url = `/products/products?p=${page}&l=${limit}`;
 
-    // TODO this is for testing only (bad connection and a slow API)
-    let data = sessionStorage.getItem(url);
-    if (!data) {
-        return instance.get(url).then(data => {
-            sessionStorage.setItem(url, JSON.stringify(data));
-            return data;
-        });
-    } else {
-        return Promise.resolve(JSON.parse(data));
-    }
+  // TODO this is for testing only (bad connection and a slow API)
+  let data = sessionStorage.getItem(url);
+  if (!data) {
+    return instance.get(url).then((data) => {
+      sessionStorage.setItem(url, JSON.stringify(data));
+      return data;
+    });
+  } else {
+    return Promise.resolve(JSON.parse(data));
+  }
 };
 
-export {listProducts};
+export { listProducts };
